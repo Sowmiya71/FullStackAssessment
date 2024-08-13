606b26ead68e8930283207631bc5ffc9e86d8867
Sowmiya71 <ceecsowmiya2025@gmail.com>
2024-08-13 09:42:53 +0530

    Add files via upload

---
+++ Java/MyClass1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class MyClass {
+ public static void main(String args[]) {
+   int employeeId;
+   string employeeName;
+   float employeeSalary;
+
+  Scanner sc = new Scanner(System.in);
+    System.out.println("Enter employee id");
+    employeeId = sc.nextInt();
+    System.out.println("Enter employee name");
+    employeeName = sc.nextInt();
+    System.out.println("Enter employee salary");
+    employeeSalary = sc.nextInt();
+
+    System.out.println("Employee id :" + employeeId);
+    System.out.println("Employee name :" + employeeName);
+    System.out.println("Employee salary :" + employeeSalary);
+  }
+}
+
+
+
+
+ 
